9e19ef4665553ef290d838fa8261bbece7c073ff
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2024-01-05 11:55:57 +0530

    REACT BASIC STRUCTURE ADDED

---
+++ REACT/01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Try from "./Try"
+
+function App() {
+
+  return (
+    <>
+    <p>NEW PARA</p>
+    <h1>HELLO REACT</h1>
+    <Try />
+    </>
+  );
+}
+
+export default App
---
+++ REACT/01vitereact/src/Try.jsx
@@ -0,0 +1,7 @@
+// NAME YOUR COMPONENTS WITH FIRST LETTER CAPITALISED AND THE EXTENSION SHOULD BE .jsx WHEN USING VITE( .js WHEN USING BASIC create react )
+function Try() {
+    return (
+        <h2>CUSTOM FILE</h2>
+    );
+}
+export default Try
